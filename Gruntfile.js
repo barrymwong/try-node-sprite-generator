a46b6c8013b1e7fe872e3ef5a603df3d0877253c
Barry Wong <barry@barrymwong.com>
2013-12-04 01:17:50 -0800

    update readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,45 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('node-sprite-generator');
+    grunt.loadNpmTasks('node-spritesheet');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        spriteGenerator: {
-            sprite: {
-                src: [
-                    'sprite_src/1x/*.png'
-                ],
-                spritePath: 'public/images/sprite.png',
-                stylesheetPath: 'less/sprite.less',
-                layout: 'vertical',
-                layoutOptions: {
-                    padding: 2
+        spritesheet: {
+            compile: {
+                options: {
+                    outputCss: 'public/css/sprite.css',
+                    output: {
+                        legacy: {
+                            pixelRatio: 1,
+                            outputImage: 'public/images/sprite.png',
+                            httpImagePath: '../images/sprite.png',
+                            selector: '.icons',
+                            // Just process the non-retina files
+                            filter: function( fullpath ) {
+                                return fullpath.indexOf( "-2x" ) === -1;
+                            },
+                            resolveImageSelector: function( name, fullpath ) {
+                                return 'icons-' + name + '::before';
+                            }
+                        },
+                        retina: {
+                            pixelRatio: 2,
+                            outputImage: 'public/images/sprite@2x.png',
+                            httpImagePath: '../images/sprite@2x.png',
+                            selector: '.icons ~ ::before',
+                            // Just process the retina files
+                            filter: function( fullpath ) {
+                                return fullpath.indexOf( "-2x" ) >= 0;
+                            },
+                            resolveImageSelector: function( name, fullpath ) {
+                                return 'icons-' + name + '::before';
+                            }
+                        }
+                    }
                 },
-                stylesheet: 'less',
-                stylesheetOptions: {
-                    prefix: 'icons',
-                    pixelRatio: 1,
-                    spritePath: '../images/sprite.png',
-                }
-            },
-            sprite2x: {
-                src: [
-                    'sprite_src/2x/*.png'
-                ],
-                spritePath: 'public/images/sprite2x.png',
-                stylesheetPath: 'less/sprite2x.less',
-                layout: 'vertical',
-                layoutOptions: {
-                    padding: 2
-                },
-                stylesheet: 'less',
-                stylesheetOptions: {
-                    prefix: '2x-icons',
-                    pixelRatio: 2,
-                    spritePath: '../images/sprite2x.png'
+                files: {
+                    '': 'sprite_src/*.png'
                 }
             }
         },
@@ -59,6 +61,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', ['spriteGenerator', 'less', 'watch']);
-    grunt.registerTask('sprite', ['spriteGenerator']);
+    grunt.registerTask('default', ['spritesheet', 'less', 'watch']);
+    grunt.registerTask('sprite', ['spritesheet']);
 };
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-TEST
+node-spritesheet
 =====
 
-brew install cairo
-
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/X11/lib/pkgconfig
-
-npm install canvas
-
-npm install node-sprite-generator
